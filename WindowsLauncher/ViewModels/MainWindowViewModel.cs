b216fad812bd70164b12b6155fcb099a0321fc5e
Jonas Droste <mail@jonas-droste.com>
2022-01-12 20:24:56 +0100

    add initial version with data binding

---
+++ WindowsLauncher/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Toolkit.Mvvm;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+
+namespace WindowsLauncher.ViewModels
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+        public MainWindowViewModel()
+        {
+            ApplicationName = "Hello Wordl";
+            ChangeApplicationNameCommand = new AsyncRelayCommand(ChangeApplicationName);
+        }
+
+        private string? applicationName;
+
+        public string? ApplicationName
+        {
+            get { return applicationName; }
+            set => SetProperty(ref applicationName, value);  
+        }
+
+        public IAsyncRelayCommand ChangeApplicationNameCommand { get; }
+
+        private Task<string> ChangeApplicationName()
+        {
+            return Task.Run(() => ApplicationName ="Changed Application Name");
+        }
+
+    }
+}
